5ce8454cc136539dfd0336a2a8d242457d062fba
firdovsi <firdovsirz@gmail.com>
2025-05-12 13:50:25 +0400

    host commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_cors import CORS
 from extentions.db import db
@@ -31,4 +32,5 @@ def main_app():
 
 if __name__ == '__main__':
     app = main_app()
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
