b242a390826652b385be4b9a38edb8c94c65f798
Firdovsirz <146226087+Firdovsirz@users.noreply.github.com>
2025-06-13 17:01:08 +0400

    Update app.py

--- app.py
+++ app.py
@@ -49,4 +49,4 @@ def main_app():
 if __name__ == '__main__':
     app = main_app()
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
